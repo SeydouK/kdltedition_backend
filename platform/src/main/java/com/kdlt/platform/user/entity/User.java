943af7e218ad7ce9efeaa625442a29ecd45a411e
Seydou <Skabore2020@gmail.com>
2026-08-29 14:09:42 +0000

    Correction

--- platform/src/main/java/com/kdlt/platform/user/entity/User.java
+++ platform/src/main/java/com/kdlt/platform/user/entity/User.java
@@ -18,7 +18,7 @@ public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false, name="photo_url")
+    @Column(nullable = true, name="photo_url")
     private String photoUrl;
     @Column(nullable = false)
     private String email;
